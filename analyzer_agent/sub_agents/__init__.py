173355922f6dccf6778b7e3f26a83a01e3551e6b
blimacardoso <blimacardoso@gmail.com>
2025-07-15 21:01:55 +0000

    feat(electric-engineer): it creates the electric engineer agent responsible for creating the power quality complience report draft

--- analyzer_agent/sub_agents/__init__.py
+++ analyzer_agent/sub_agents/__init__.py
@@ -2,8 +2,10 @@
 
 Atrributes:
     data_scientst_agent: Senior Data Scientist specializing in electrical power quality data from devices like PowerNET PQ-600 G4.
+    electric_engineer_agent: Senior Electric Engineer specializing in electrical power quality data from devices like PowerNET PQ-600 G4and ANEEL regulations, responsible for generating a detailed and well-structured technical compliance report.
 """
 
 from .data_scientist_agent import data_scientst_agent
+from .electric_engineer_agent import electric_engineer_agent
 
-__all__ = ["data_scientst_agent"]
+__all__ = ["data_scientst_agent", "electric_engineer_agent"]
